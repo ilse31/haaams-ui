35f02bfd2465095f6a00eddcdd59b5866914b996
ilse31 <ilhamprazsetyo33@gmail.com>
2024-06-24 22:52:45 +0700

    v0.0.13

--- src/components/UploadFile/FilePreviewProps.tsx
+++ src/components/UploadFile/FilePreviewProps.tsx
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import { FileWithPreview } from '../../libs/dropzone';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import * as React from "react";
+import { FileWithPreview } from "../../libs/dropzone";
 
 export type FilePreviewProps = {
-	file: FileWithPreview;
+  file: FileWithPreview;
 } & (
-	| {
-			deleteFile?: (
-				e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-				file: FileWithPreview
-			) => void;
-			readOnly?: true;
-	  }
-	| {
-			deleteFile: (
-				e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-				file: FileWithPreview
-			) => void;
-			readOnly?: false;
-	  }
+  | {
+      deleteFile?: (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+        file: FileWithPreview
+      ) => void;
+      readOnly?: true;
+    }
+  | {
+      deleteFile: (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+        file: FileWithPreview
+      ) => void;
+      readOnly?: false;
+    }
 );
